add saving log, fixed modified mlp, added losses from gradient enhanced, added scheduler for learning rate

main.py
@@ -16,17 +16,21 @@ import src.tools.plot_tools as plot_tools
 import src.tools.util_func as util_func
 from datetime import datetime
 
-def train_and_save_data(model, device, config, save_dir, epochs, non_dim, forward_transform_input, forward_transform_output):
+def train_and_save_data(model, device, config, save_dir, epochs, non_dim,
+                        log_interval, update_interval,
+                        forward_transform_input, forward_transform_output):
     train_data = train.Train_Loop_data(
         model=model,
         device=device,
         nondim_input=forward_transform_input,
         nondim_output=forward_transform_output,
+        refresh_bar=update_interval,
+        log_interval=log_interval,
     )
 
     intermediate_model = train_data.train_pinn(
         config=config,
-        log_path=save_dir,
+        save_path=save_dir,
         train_prop=0.01,
         nu=0.01,
         epochs=epochs,
@@ -61,6 +65,11 @@ def train_and_save_data(model, device, config, save_dir, epochs, non_dim, forwar
     loss_history_train, loss_history_test = train_data.get_loss_history()
     plot_tools.plot_loss_history({"train_loss": loss_history_train, "val_loss": loss_history_test}, save_dir=save_dir, additional_name="_intermediate_model")
 
+    train_data.save_model(
+        path=save_dir,
+        additional_name="_intermediate_model",
+    )
+
     return intermediate_model
 
 def train_and_save_nodata(model, device, config, save_dir, epochs, non_dim, forward_transform_input, forward_transform_output):
@@ -74,7 +83,7 @@ def train_and_save_nodata(model, device, config, save_dir, epochs, non_dim, forw
 
     final_model = train_obj.train_pinn(
         config=config,
-        log_path=save_dir,
+        save_path=save_dir,
         train_prop=0.4,
         nu=0.01,
         epochs=epochs,
@@ -122,7 +131,7 @@ def main(args):
     print(f"Using device: {device}")
 
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    save_dir = f'./results/{timestamp}_dimension'
+    save_dir = f'./results/{timestamp}'
 
     util_func.save_args(args, save_dir=save_dir)
 
@@ -134,7 +143,7 @@ def main(args):
         forward_transform_input, forward_transform_output = util_func.get_non_dim_transform()
 
 
-    layer = [args.in_dim_RFF] + [args.features] * args.n_layers + [3]
+    layer = [args.in_dim] + [args.features] * args.n_layers + [3]
     if args.model == 'mlp':
         PINN = model.PINN_linear(layer, RFF = args.RFF, hard_constraint=None, activation=nn.Tanh, device = device)
     elif args.model == 'modified_mlp':
@@ -148,10 +157,12 @@ def main(args):
         epochs=args.epochs_data,
         non_dim=non_dim,
         forward_transform_input=forward_transform_input,
-        forward_transform_output=forward_transform_output
+        forward_transform_output=forward_transform_output,
+        update_interval=args.update_iter,
+        log_interval=args.log_iter
     )
 
-    if args.no_data_refine:
+    if args.pde_refine:
         print("Finished training the intermediate model. Gonna start refining using pde and equations")
 
         final_model = train_and_save_nodata(
@@ -189,22 +200,24 @@ if __name__ == '__main__':
     # parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
     parser.add_argument('--non_dim', type=bool, default=False, help='whether to use non-dimensionalization')
     # parser.add_argument('--batch_size', type=int, default=256, help='batch size')
-    parser.add_argument('--no_data_refine', type=bool, default=False, help='whether to use no data to refine the model')
+    parser.add_argument('--pde_refine', type=bool, default=False, help='whether to use no data to refine the model')
     parser.add_argument('--epochs_data', type=int, default=5000, help='training epochs for data')
     parser.add_argument('--epochs_nodata', type=int, default=2000, help='training epochs for no data')
 
     # model settings
-    parser.add_argument('--model', type=str, default='mlp', choices=['mlp', 'modified_mlp'], help='type of mlp')
+    parser.add_argument('--model', type=str, default='mlp', choices=['mlp', 'modified_mlp', 'saved'], help='type of mlp')
     parser.add_argument('--n_layers', type=int, default=4, help='the number of layer')
     parser.add_argument('--features', type=int, default=256, help='feature size of each layer')
     parser.add_argument('--RFF', type=bool, default=False, help='whether to use Random Fourier Features')
     parser.add_argument('--in_dim', type=int, default=3, help='size of model input, might not be 3 if using RFF')
-
+    parser.add_argument('--saved_model', type=str, default=None, help='path to saved model')
 
     # log settings
-    parser.add_argument('--log_iter', type=int, default=1000, help='print log every...')
-    parser.add_argument('--update_iter', type=int, default=50000, help='update progress bar every...')
+    parser.add_argument('--log_iter', type=int, default=50, help='print log every...')
+    parser.add_argument('--update_iter', type=int, default=500, help='update progress bar every...')
 
     args = parser.parse_args()
+
+    assert args.model != 'saved' or args.saved_model is not None, "Please provide a saved model path if using the saved model option."
 
     main(args)

src/models/loss.py
@@ -20,6 +20,7 @@ def navier_stokes(txy_col, output, nu = 0.01, non_dim = False, eval=False):
     Time-dependent Navier-Stokes PDE residual.
     Lets suppose that nu = 0.01 is not changed (otherwise change util_func.py)
     """
+
     Re = util_func.get_Reynolds()
 
     # PDE Residual Loss
@@ -58,6 +59,10 @@ def navier_stokes(txy_col, output, nu = 0.01, non_dim = False, eval=False):
         u_out = du_dx*nu - p
         v_out = dv_dx
 
+    ## TEST de gradient-enhanced
+    d_momentum_x = torch.autograd.grad(momentum_x, txy_col, grad_outputs=torch.ones_like(momentum_x), create_graph=True)[0]
+    d_momentum_x_dt, d_momentum_x_dx, d_momentum_x_dy = d_momentum_x[:, 0], d_momentum_x[:, 1], d_momentum_x[:, 2]
+
     # Detach gradients if eval
     if eval:
         du = du.detach()
@@ -70,10 +75,13 @@ def navier_stokes(txy_col, output, nu = 0.01, non_dim = False, eval=False):
         continuity = continuity.detach()
         momentum_x = momentum_x.detach()
         momentum_y = momentum_y.detach()
+        d_momentum_x_dt = d_momentum_x_dt.detach()
+        d_momentum_x_dx = d_momentum_x_dx.detach()
+        d_momentum_x_dy = d_momentum_x_dy.detach()
         u_out = u_out.detach()
         v_out = v_out.detach()
 
-    return continuity, momentum_x, momentum_y, u_out, v_out
+    return continuity, momentum_x, momentum_y, d_momentum_x_dt, d_momentum_x_dx, d_momentum_x_dy, u_out, v_out
 
 class Loss:
     ### An object that we're gonna reset at each epoch
@@ -118,10 +126,15 @@ class Loss:
         self.loss_total.zero_() 
 
     def compute_pde_loss(self, txy_col, output, outflow_eq = True, eval = False,):
-        continuity, momentum_x, momentum_y, u_out, v_out = navier_stokes(txy_col, output, self.nu, self.non_dim, eval = eval)
-        tmp = torch.mean(continuity**2) + torch.mean(momentum_x**2) + torch.mean(momentum_y**2) + outflow_eq * torch.mean(u_out**2) + outflow_eq * torch.mean(v_out**2)
+        continuity, momentum_x, momentum_y, d_momentum_x_dt, d_momentum_x_dx, d_momentum_x_dy, u_out, v_out = navier_stokes(txy_col, output, self.nu, self.non_dim, eval = eval)
+        tmp = (torch.mean(continuity**2) + 
+               torch.mean(momentum_x**2) + 
+               torch.mean(momentum_y**2) + 
+               torch.mean(d_momentum_x_dt**2) +
+               torch.mean(d_momentum_x_dx**2) +
+               torch.mean(d_momentum_x_dy**2) +
+               outflow_eq * torch.mean(u_out**2) + outflow_eq * torch.mean(v_out**2))
         return tmp.to(self.device)
-
 
     def pde_loss(self, txy_col, output, outflow_eq = True):
         assert self.loss_pde_tmp==0 ## otherwise we need to call new_batch before it 
@@ -142,19 +155,19 @@ class Loss:
         self.loss_data_tmp = tmp
         return tmp
     
-    def compute_boundary_loss(self, txy_col, output):
+    def compute_boundary_loss(self, output, boundary):
         """
-        txy_col: [N, 3] tensor of points in the domain
         output: [N, 3] tensor of outputs at those points
         boundary: [N, 3] tensor of boundary conditions at those points
+
+        We suppose that the output is already in the right format
         """
-        boundary = torch.zeros_like(output).to(self.device)
         tmp = nn.MSELoss()(output, boundary).to(self.device)
         return tmp
     
-    def boundary_loss(self, txy_col, output):
+    def boundary_loss(self, output, boundary):
         assert self.loss_boundary_tmp==0
-        tmp = self.compute_boundary_loss(txy_col, output)
+        tmp = self.compute_boundary_loss(output, boundary)
         self.loss_boundary += tmp
         self.loss_boundary_tmp = tmp
         return tmp
@@ -259,7 +272,7 @@ class Loss:
     def update_lambda(self):
         """
         Update the lambda values using the formula in the expert guide paper
-        Do it every n (n=1000 typically) epochs
+        Do it every n (n=1000 e.g.) epochs
         """
         norm_loss_data = self.get_grad_loss(self.loss_data_tmp)
         norm_loss_pde = self.get_grad_loss(self.loss_pde_tmp)

src/models/train.py
@@ -16,6 +16,7 @@ from time import time
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
 from src.tools.plot_tools import evaluate_model
+from torch.optim.lr_scheduler import ExponentialLR
 
 import pdb
 
@@ -25,7 +26,7 @@ class Train_Loop(ABC):
     Abstract class for training loops.
     """
 
-    def __init__(self, model, device, need_loss, lambda_list, nu, alpha, nondim_input = None, nondim_output = None,refresh_bar = 200):
+    def __init__(self, model, device, need_loss, lambda_list, nu, alpha, nondim_input = None, nondim_output = None, refresh_bar = 500, log_interval = 50):
 
 
         self.model = model
@@ -34,6 +35,7 @@ class Train_Loop(ABC):
         self.nu = nu
         self.alpha = alpha
         self.refresh_bar = refresh_bar
+        self.log_interval = log_interval
         self.nondim_input = nondim_input
         self.nondim_output = nondim_output
         self.batch_size = 8000  # Define batch size
@@ -46,6 +48,8 @@ class Train_Loop(ABC):
         self.loss_obj = Loss_module.Loss(model = model, device = device, nu=nu, alpha=alpha, need_loss=need_loss, lambda_list=lambda_list)
         self.loss_history_train = []
         self.loss_history_test = []
+        
+        self.time_to_complete = f"Total time: {0.:.2f}s | Avg speed: {0.:.2f} it/s"
     
     def set_data(self, config, train_prop):
         """
@@ -69,7 +73,7 @@ class Train_Loop(ABC):
 
         return txy_col, output_data, xyt_col_test, output_data_test
 
-    def train_pinn(self, config, log_path, train_prop = 0.01, nu=0.01, epochs=10000, adapting_weight = True, additional_name = ""):
+    def train_pinn(self, config, save_path, train_prop = 0.01, nu=0.01, epochs=10000, adapting_weight = True, additional_name = ""):
         """
         Train the PINN model using the Navier-Stokes equations.
 
@@ -88,14 +92,21 @@ class Train_Loop(ABC):
         num_batches = txy_col.size(0) // batch_size + (txy_col.size(0) % batch_size != 0)
         print("num_batches: ", num_batches, "batch_size: ", batch_size)
         
+        ## cte for optim and scheduler (not hyper parameters for now)
+        lr = 1e-3
+        decay = 0.1
+        decay_steps = 2000
+
         optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+        scheduler = ExponentialLR(optimizer, gamma=(1 - decay/decay_steps))
         progress_bar = tqdm(range(epochs), desc="Training Progress", unit="epoch")
 
         loss_log: list[dict[str, float]] = []
 
         for epoch in progress_bar:
             optimizer.zero_grad()
-            self.add_log_entry(loss_log, epoch, num_batches)
+            if epoch % self.log_interval == 0 or epoch == epochs - 1:
+                self.add_log_entry(loss_log, epoch, num_batches)
             self.loss_obj.new_epoch(self.lambda_data, self.lambda_pde, self.lambda_boundary, self.lambda_initial)
             tmp_loader = batch_data(txy_col, output_data, batch_size, shuffle=False)
             for inputs, targets in tmp_loader:
@@ -112,26 +123,36 @@ class Train_Loop(ABC):
                         self.lambda_data, self.lambda_pde, self.lambda_boundary, self.lambda_initial = self.loss_obj.update_lambda()
                 self.loss_obj.backward(retain = False)
                 optimizer.step()
-
+                scheduler.step()
+            if self.lambda_data == 0.:
+                # pdb.set_trace()
+                pass
             self.loss_history_train.append(self.loss_obj.get_loss_pde().item() / num_batches)
             if epoch % self.refresh_bar == 0 or epoch == epochs - 1:
                 test_loader = batch_data(xyt_col_test, output_data_test, batch_size, shuffle=False)
                 loss_test = self.evaluate(test_loader)  # Test loss computation using known data
                 self.update_progress_bar(progress_bar, loss_test, num_batches)
                 self.loss_history_test.append((epoch,float(loss_test)))
-                self.save_log(log_path, loss_log, additional_name)
+                self.save_log(save_path, loss_log, additional_name)
+        
+
         self.loss_history_test = np.array(self.loss_history_test)
         self.loss_history_train = np.array(self.loss_history_train)
+
+        self.time_to_complete = f"Total time: {progress_bar.format_dict["elapsed"]:.2f}s | Avg speed: {epochs/progress_bar.format_dict["elapsed"]:.2f} it/s"
+        self.save_log(save_path, loss_log, additional_name)
+        self.save_time_complete(save_path, additional_name)
+
         return self.model
 
     def get_loss_history(self):
         return self.loss_history_train, self.loss_history_test
     
-    def save_model(self, path):
+    def save_model(self, path, additional_name = ""):
         """
         Save the model to a file.
         """
-        torch.save(self.model.model, os.path.join(path,'model_weights.pth'))
+        torch.save(self.model.model, os.path.join(path,'model' + additional_name + '.pth'))
     
     def save_model_weights(self, path):
         """
@@ -162,6 +183,33 @@ class Train_Loop(ABC):
         os.makedirs(path, exist_ok=True)
         pd.DataFrame(log).to_csv(os.path.join(path, "log" + additional_name + ".csv"), index=False)
 
+    def save_time_complete(self, path, additional_name = ""):
+        """
+        Save the time to complete the training.
+        """
+        os.makedirs(path, exist_ok=True)
+        with open(os.path.join(path, 'time_complete'+ additional_name+'.txt'), 'w') as f:
+            f.write(self.time_to_complete)
+    
+    def set_condition_data(self):
+        radius = 0.99
+        x = radius*torch.cos(torch.linspace(0, 2*3.14, 50)).reshape(-1,1)
+        y = radius*torch.sin(torch.linspace(0, 2*3.14, 50)).reshape(-1,1)
+        t = torch.arange(0, 150.).reshape(-1,1)
+        X, Y, T = np.meshgrid(x, y, t)
+        self.BC_geom = np.vstack((T.flatten(), X.flatten(), Y.flatten())).T
+        if self.nondim_input is not None:
+            self.BC_geom = self.nondim_input(self.BC_geom)
+        self.BC_geom = torch.tensor(self.BC_geom, dtype=torch.float32, requires_grad=True).to(self.device)
+
+        x = -20. * torch.ones((50,1))
+        y = torch.linspace(-20, 20, 50).reshape(-1,1)
+        t = torch.arange(0, 150.).reshape(-1,1)
+        X, Y, T = np.meshgrid(x, y, t)
+        self.BC_geom2 = np.vstack((T.flatten(), X.flatten(), Y.flatten())).T
+        if self.nondim_input is not None:
+            self.BC_geom2 = self.nondim_input(self.BC_geom2)
+        self.BC_geom2 = torch.tensor(self.BC_geom2, dtype=torch.float32, requires_grad=True).to(self.device)
 
     @abstractmethod
     def update_progress_bar(self, progress_bar, loss_test, num_batches):
@@ -185,9 +233,9 @@ class Train_Loop_data(Train_Loop):
     Class for training loops with data.
     """
 
-    def __init__(self, model, device, nondim_input = None, nondim_output = None, nu=0.01, alpha=0.9):
+    def __init__(self, model, device, nondim_input = None, nondim_output = None, nu=0.01, alpha=0.9, refresh_bar=500, log_interval=50):
         super().__init__(model, device,
-                         [True, True, False, False], [1., 1., 0., 0.],
+                         [True, True, False, False], [1., 1., 0., 0.], refresh_bar=refresh_bar, log_interval=log_interval,
                          nu=nu, alpha=alpha, nondim_input=nondim_input, nondim_output=nondim_output,)
 
     def compute_loss(self, inputs, outputs, targets):
@@ -239,15 +287,7 @@ class Train_Loop_nodata(Train_Loop):
                          nu=nu, alpha=alpha, nondim_input=nondim_input, nondim_output=nondim_output,)
         
         ## TO DO along with compute_loss for BC
-        radius = 0.99
-        x = radius*torch.cos(torch.linspace(0, 2*3.14, 50)).reshape(-1,1)
-        y = radius*torch.sin(torch.linspace(0, 2*3.14, 50)).reshape(-1,1)
-        t = torch.arange(0, 150.).reshape(-1,1)
-        X, Y, T = np.meshgrid(x, y, t)
-        self.BC_geom = np.vstack((T.flatten(), X.flatten(), Y.flatten())).T
-        if nondim_input is not None:
-            self.BC_geom = nondim_input(self.BC_geom)
-        self.BC_geom = torch.tensor(self.BC_geom, dtype=torch.float32, requires_grad=True).to(device)
+        self.set_condition_data()
         
         X,Y = import_data(init_path, nondim_input, nondim_output)
         if nondim_input is not None:
@@ -265,8 +305,13 @@ class Train_Loop_nodata(Train_Loop):
 
         ## TO DO : make it cleaner and integrated in the framework
         ## FOR NOW just use predefined boundary conditions
-        outputs = self.model(self.BC_geom)
-        self.loss_obj.boundary_loss(self.BC_geom, outputs)
+        outputs_circle = self.model(self.BC_geom)
+        circle_bc = torch.zeros_like(outputs_circle).to(self.device)
+        outputs_line = self.model(self.BC_geom2)
+        line_bc = torch.ones_like(outputs_line).to(self.device)
+        outputs = torch.cat((outputs_circle, outputs_line), dim=0)
+        boundary = torch.cat((circle_bc, line_bc), dim=0)
+        self.loss_obj.boundary_loss(outputs, boundary)
         outputs = self.model(self.init_data)
         self.loss_obj.initial_loss(self.init_data, outputs, self.init_target)
 